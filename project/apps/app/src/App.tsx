3fa8671c119d67746caab8ab3d586a67c58d7e2c
Aman Khan <lastnamekhan47@gmail.com>
2024-12-22 23:03:31 +0530

    task-1

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -1,12 +1,28 @@
+import React from 'react';
 import { List } from 'ui'
 
 const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
 
 const App = () => {
+  const [pokemonList, setPokemonList] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        const response = await fetch(api);
+        const data = await response.json();
+        setPokemonList(data.results);
+      } catch (error) {
+        console.error("Error fetching Pokémon data:", error);
+      }
+    };
+
+    fetchPokemon();
+  }, []);
   return (
   <>
     <h1>Pokemon list:</h1>
-    <List />
+    <List pokemonList={pokemonList} />
   </>
   )
 }
--- project/packages/ui/components/List.tsx
+++ project/packages/ui/components/List.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
 
-export const List = () => {
-  return <p>List</p>
+interface MyProps {
+  name: string;
+  url: string;
+}
+
+export const List = ({ pokemonList }: { pokemonList: MyProps[] }) => {
+  return (
+    <>    
+    <ul>
+    {pokemonList.map((pokemon, index) => (
+      <li key={index}>{pokemon.name}</li>
+    ))}
+  </ul>
+  </>
+  )
 }
